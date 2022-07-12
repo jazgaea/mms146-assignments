bcfefc37ccf1ce5463d2d27263b7ea1617089ecf
Nicko Buenviaje <74237653+FalsePatronus@users.noreply.github.com>
2022-07-12 14:07:37 +0800

    Update Object1.py

--- Object1.py
+++ Object1.py
@@ -12,10 +12,10 @@ class Cabinet:
    
   #For Opening and closing the cabinet
     def open_shelf(self, sh):
-        self.shelves = "Shelf is opened."
+        self.shelves = "A shelf is opened."
   
     def close_shelf(self, sh):
-        self.shelves = "Shelf is closed."
+        self.shelves = "the shelf is now closed."
     
   #changing the handles
     def change_handles(self, ht):
@@ -24,14 +24,40 @@ class Cabinet:
  #Let's experiment by opening the shelf, adding an item, then closing it again.
   
     def add_item(self, sh):
-        self.shelves = open_shelf + "You added an item" + close_shelf
+        self.shelves =  "You stored an item."
     
   #Okay, let's start reviewing our code! 
   #Our Cabinet is 5 meters tall, paint is dark brown, handles are gold, made of narra, and all shelves are closed.
 
-cabinet1 = Cabinet("5 meters", "Dark Brown", "Gold", "Narra", "Shelf is closed.")
+cabinet1 = Cabinet("5 meters tall, ", "Dark Brown", " Gold handles, ", "made of Narra, ", "Shelves are closed.")
   
-  #Let's repaint it to pink.
-print ("I don't like a " + cabinet1.color + " cabinet")
+  #This is for calling all the attributes of the Cabinet.
+print ("The original attributes of our cabinet is " + cabinet1.size + cabinet1.color + "," + cabinet1.handleType + cabinet1.material + "all of " + cabinet1.shelves)
+
+  #Let's repaint the color to pink.
+print ("But I don't like a " + cabinet1.color + " cabinet")
 cabinet1.change_Color("pink")
 print ("let's paint it to " + cabinet1.color)
+
+  #Let's open and close the shelves.
+print ("Before changing the code, all the " + cabinet1.shelves)
+cabinet1.open_shelf(" ")
+print ("after changing the code, " + cabinet1.shelves)
+cabinet1.close_shelf(" ")
+print ("By changing the code again, " + cabinet1.shelves)
+
+  #Now let's change the handle type
+print ("The original attribute of the cabinet is " + cabinet1.handleType)
+cabinet1.change_handles("diamond handles")
+print ("Now it is " + cabinet1.handleType + ".")
+
+  #This one is an experiment code. We would like to display every changes made.
+print ("The new attributes of our cabinet are " + cabinet1.size + cabinet1.color + " paint, "+ cabinet1.handleType + ", " + cabinet1.material + cabinet1.shelves)
+
+  #Let's try to add an item to the cabinet
+cabinet1.open_shelf(" ")
+print ("TO add an item, first, " + cabinet1.shelves)
+cabinet1.add_item(" ")
+print ("If you put an item, " + cabinet1.shelves)
+cabinet1.close_shelf(" ")
+print ("Then, of course, push the drawer so that " + cabinet1.shelves)
