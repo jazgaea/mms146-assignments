930b96fd1152e0e0c83bfc0400c3ad26e7b1db21
Nicko Buenviaje <74237653+FalsePatronus@users.noreply.github.com>
2022-07-13 11:28:17 +0800

    Create Object2.py

---
+++ Object2.py
@@ -0,0 +1,22 @@
+class Table:
+  def __init__(self, s, m, h, w, g):
+    self.size = s
+    self.material = m
+    self.height = h
+    self.weight = w
+    self.glass = g
+
+#Let's try an if-return statement
+  def remove_glass(self):
+    if (self.glass = "With glass")
+    return ("Glass removed."
+
+#Let's assign the attributes to two objects
+table1 = Table("large", "Metal", "1 meter", "20 KG", "No glass")
+table2 = Table("small", "Wood", "0.50 meter", "15 KG", "With glass")
+
+print ("table1 is made of " + table1.material)
+print ("table2 is " + table2.height + " tall.")
+print ("table2 is " + table2.glass)
+table2.remove_glass
+print ("table2 has now its " + table2.glass)
